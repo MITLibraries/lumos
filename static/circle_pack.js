680a3a130c81ea423b016df8765cdb937957c2e2
Helen Bailey <hakbailey@gmail.com>
2015-06-17 18:13:31 -0400

    Remove extra code

--- static/circle_pack.js
+++ static/circle_pack.js
@@ -85,7 +85,7 @@ function createGraph() {
 			})
 			.on("mouseover", function(d, i) {
 				circle.classed("faded_node", function(e, j) {
-					if (d != e.name || !circle.classed("node-root")) {
+					if (d != e.name) {
 						return true;
 					}
 					else {return false}
